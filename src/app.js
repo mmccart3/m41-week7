7a365312ca4d5261b9d2ff036e730efd1ab421ea
philomeno <106666729+philomeno@users.noreply.github.com>
2022-10-10 15:11:50 +0100

    yargs functionality added

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
+const yargs = require("yargs")
 const Movie = require("./utils");
 
-// console.log(process.argv)
-
-// if (process.argv[2] === "harry") {
-//     console.log("Hello Mr. Potter")
-// }
-
-function app (argv) {
-    switch (argv[2]){
-        case "add":
-            // const movie = [argv[3],argv[4]];
-            // console.log (movie);
-            const newMovie = new Movie(argv[3],argv[4]);
-            newMovie.add();
-            break;
-        case "addMulti":
-            const Movie1 = new Movie(argv[3],argv[4]);
+function app (argument) {
+    if (argument.add) {
+        const newMovie = new Movie(argument.movie,argument.actor);
+        newMovie.add()}
+    else if (argument.addMulti) {
+            const Movie1 = new Movie(argument.movie1,argument.actor1);
             Movie1.add();
-            const Movie2 = new Movie(argv[5],argv[6]);
+            const Movie2 = new Movie(argument.movie2,argument.actor2);
             Movie2.add();
-            const Movie3 = new Movie(argv[7],argv[8]);
+            const Movie3 = new Movie(argument.movie3,argument.actor3);
             Movie3.add();
-            break;
-        default:
+    } else {
             console.log ("I do not recognise that command");
-            break;
     }
 };
 
-app(process.argv);
+app(yargs.argv);
